b700f6fd99c9a30455d891e622f70c1d5adadfc6
juangr18 <juanr970318@gmail.com>
2022-03-07 08:53:49 -0500

    Create components in frontend and service email send

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { SendEmailComponent } from './home/send-email/send-email.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SendEmailComponent
   ],
   imports: [
     BrowserModule,
